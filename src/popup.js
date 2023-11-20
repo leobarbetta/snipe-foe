527da673c30438bac9cc0617f0af651406bb2c74
leobarbetta <leonardo.barbetta@smtcode.com>
2023-11-20 20:12:22 -0300

    Feat: Focus when open and enter trigger action

--- src/popup.js
+++ src/popup.js
@@ -1,16 +1,30 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const submitButton = document.getElementById('submitButton');
-  
-  submitButton.addEventListener('click', function() {
-    const input1 = document.getElementById('input1').value;
-    const input2 = document.getElementById('input2').value;
+  const fpsToLevelInput = document.getElementById('fps-to-level');
+  const fpsDesiredPositionInput = document.getElementById('fps-desired-position');
 
-    let FPsToSnipe = Number(input1) / 2;
+  fpsToLevelInput.focus();
 
-    if(Number(input2) !== 0){
-      FPsToSnipe = FPsToSnipe +(Number(input2)/2)
+  fpsToLevelInput.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) submitButton.click();
+  });
+
+  fpsDesiredPositionInput.addEventListener("keyup", function (event) {
+    event.preventDefault();
+    if (event.keyCode === 13) submitButton.click();
+  });
+
+  submitButton.addEventListener('click', function () {
+    const fpsToLevel = fpsToLevelInput.value;
+    const fpsDesiredPosition = fpsDesiredPositionInput.value;
+
+    let FPsToSnipe = Number(fpsToLevel) / 2;
+
+    if (Number(fpsDesiredPosition) !== 0) {
+      FPsToSnipe = FPsToSnipe + (Number(fpsDesiredPosition) / 2)
     }
-    
+
     document.getElementById('outputLabel').textContent = `FPs to snipe: ${FPsToSnipe}`;
   });
 });
